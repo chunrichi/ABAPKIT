2b4b72b16c7089593dc065824410ebd6c23684c3
旧事 <545049148@qq.com>
2023-11-21 14:39:41 +0800

    🚧 快速添加图片

--- tools/akit_tsdoc/main.go
+++ tools/akit_tsdoc/main.go
@@ -54,8 +54,19 @@ func main() {
 
 		comment := c.PostForm("comment")
 
+		// 检查是否有通过Ctrl+V粘贴的图片
+		fileHeader, err := c.FormFile("image")
+		if err != nil {
+			// 如果没有通过Ctrl+V粘贴的图片，则尝试从request body中读取
+			fileHeader, err = c.FormFile("image[]")
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "No image file provided"})
+				return
+			}
+		}
+
 		// 处理上传的文件
-		file, header, err := c.Request.FormFile("image")
+		file, err := fileHeader.Open()
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
@@ -63,7 +74,7 @@ func main() {
 		defer file.Close()
 
 		// 生成唯一的文件名
-		filename := fmt.Sprintf("%d_%s", header.Size, header.Filename)
+		filename := fmt.Sprintf("%d_%s", fileHeader.Size, fileHeader.Filename)
 
 		// 保存文件到服务器
 		out, err := os.Create(filepath.Join("uploads", filename))
